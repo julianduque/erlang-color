afb17a8cb05fcff18721c4cae173ceefbadad911
Julian Duque <julianduquej@gmail.com>
2014-01-18 01:26:25 -0500

    Add test for bright colors

--- test/color_test.erl
+++ test/color_test.erl
@@ -27,3 +27,28 @@ cyan_test() ->
 white_test() ->
   ?assertEqual("\e[37mwhite\e[0m", color:white("white")).
 
+%% Bright Colors
+blackb_test() ->
+  ?assertEqual("\e[30;1mblack\e[0m", color:blackb("black")).
+
+redb_test() ->
+  ?assertEqual("\e[31;1mred\e[0m", color:redb("red")).
+
+greenb_test() ->
+  ?assertEqual("\e[32;1mgreen\e[0m", color:greenb("green")).
+
+yellowb_test() ->
+  ?assertEqual("\e[33;1myellow\e[0m", color:yellowb("yellow")).
+
+blueb_test() ->
+  ?assertEqual("\e[34;1mblue\e[0m", color:blueb("blue")).
+
+magentab_test() ->
+  ?assertEqual("\e[35;1mmagenta\e[0m", color:magentab("magenta")).
+
+cyanb_test() ->
+  ?assertEqual("\e[36;1mcyan\e[0m", color:cyanb("cyan")).
+
+whiteb_test() ->
+  ?assertEqual("\e[37;1mwhite\e[0m", color:whiteb("white")).
+
